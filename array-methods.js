8d29618f548ef9c86406f2250481bc5c262f852f
David Cramb <david.cramb@gmail.com>
2016-02-23 21:45:14 -0600

    includes e planets array done

--- array-methods.js
+++ array-methods.js
@@ -9,20 +9,22 @@ planets.forEach(function(planet) {
   el.innerHTML += planet + '<br>';
 });
 // Use the map method to create a new array where the first letter of each planet is capitalized
-// MAP METHOD
 
-// var reversedColors = colors.map(
-//   function(placeholder){
-//     return placeholder.split("").reverse().join("");
-//   }
-//   )
-
+// The map method saves a new array into capPlanets (Go Planet!)
+// a blank .charAt() method grabs the first letter of each string in the array and capitalizes it, then adds the rest of 
+// the string on itself with the slice method, subtracting the first (lowercase) letter.
 var capPlanets = planets.map(
   function(capitalized){
     return capitalized.charAt().toUpperCase() + capitalized.slice(1);
-
-  })
+  });
 // Use the filter method to create a new array that contains planets with the letter 'e'
+
+//Using the include method inside the filter method creates a new array of planets that contain the letter 'e'
+var ePlanets = planets.filter(
+  function(planet){
+    return planet.includes('e');
+  })
+console.log(ePlanets)
 
 // Use the reduce method to create a sentence from the words in the following array
 
